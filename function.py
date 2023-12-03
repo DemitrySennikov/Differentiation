a4318dc28abce87a582a8429daa9481cb73e4d33
sergey.kheyfets <telegram only>
2023-12-04 02:52:53 +0500

    солнце взойдёт

--- function.py
+++
@@ -1,6 +0,0 @@
-class Function():
-    def __init__(self):
-        self.const = 1
-        self.func = None
-        self.base = None
-        self.arg = None
--- syntax.py
+++
@@ -1,2 +0,0 @@
-def syntax_checking(expression):
-    pass
